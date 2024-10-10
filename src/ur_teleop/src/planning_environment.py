2b6b2ad3bac7338a22c15c25b5b8b8552efeaf48
FarStryke21 <aman.chulawala@gmail.com>
2024-10-10 11:20:25 -0400

    Added protection cage for teleop motion

--- src/ur_teleop/src/planning_environment.py
+++ src/ur_teleop/src/planning_environment.py
@@ -4,20 +4,16 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from moveit_commander import PlanningSceneInterface
 
-class GroundPlaneAdder:
+class CageAdder:
     def __init__(self):
-        rospy.init_node('ground_plane_adder', anonymous=True)
+        rospy.init_node('cage_adder', anonymous=True)
         self.scene = PlanningSceneInterface()
         rospy.sleep(1.0)  # Wait for the planning scene to initialize
 
     def clear_scene(self):
-        # Get the names of all objects in the scene
         object_names = self.scene.get_known_object_names()
-        
-        # Remove all objects
         for obj in object_names:
             self.scene.remove_world_object(obj)
-        
         rospy.loginfo("Cleared all objects from the planning scene")
         rospy.sleep(1.0)  # Wait for the scene to update
 
@@ -29,18 +25,40 @@ class GroundPlaneAdder:
         pose.pose.position.z = -0.01
         pose.pose.orientation.w = 1.0
 
-        # Add a plane with name 'ground', normal pointing up (z-axis), and offset of 0
         self.scene.add_plane("ground", pose, normal=(0, 0, 1), offset=0)
         rospy.loginfo("Ground plane added to the planning scene")
+
+    def add_cage_walls(self):
+        cage_height = 1.0
+        cage_size = 1.5
+        wall_thickness = 0.01
+
+        # Add walls
+        self.add_box("wall_front", 0, cage_size/2, cage_height/2, cage_size, wall_thickness, cage_height)
+        self.add_box("wall_back", 0, -cage_size/2, cage_height/2, cage_size, wall_thickness, cage_height)
+        self.add_box("wall_left", -cage_size/2, 0, cage_height/2, wall_thickness, cage_size, cage_height)
+        self.add_box("wall_right", cage_size/2, 0, cage_height/2, wall_thickness, cage_size, cage_height)
+
+        rospy.loginfo("Cage walls added to the planning scene")
+
+    def add_box(self, name, x, y, z, size_x, size_y, size_z):
+        pose = PoseStamped()
+        pose.header.frame_id = "base_link"
+        pose.pose.position.x = x
+        pose.pose.position.y = y
+        pose.pose.position.z = z
+        pose.pose.orientation.w = 1.0
+        self.scene.add_box(name, pose, (size_x, size_y, size_z))
 
     def run(self):
         self.clear_scene()
         self.add_ground_plane()
+        self.add_cage_walls()
         rospy.spin()
 
 if __name__ == '__main__':
     try:
-        ground_plane_adder = GroundPlaneAdder()
-        ground_plane_adder.run()
+        cage_adder = CageAdder()
+        cage_adder.run()
     except rospy.ROSInterruptException:
         pass
