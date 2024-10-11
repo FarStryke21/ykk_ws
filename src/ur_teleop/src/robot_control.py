8aedbf386d1c0c8e4b80acef512f2c1d3e7f56e3
Aman Chulawala <aman.chulawala@gmail.com>
2024-10-11 14:40:54 +0000

    spacemouse sim supprt

--- src/ur_teleop/src/robot_control.py
+++ src/ur_teleop/src/robot_control.py
@@ -2,7 +2,7 @@
 
 import rospy
 import moveit_commander
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import TwistStamped, Pose
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from threading import Lock
 import sys
@@ -14,9 +14,8 @@ class MoveItController:
         moveit_commander.roscpp_initialize(sys.argv)
         self.robot = moveit_commander.RobotCommander()
         planning_grp = "arm"
-        # planning_grp = "manipulator"
         self.group = moveit_commander.MoveGroupCommander(planning_grp)
-        self.cmd_sub = rospy.Subscriber('joystick_commands', Twist, self.cmd_callback, queue_size=1)
+        self.cmd_sub = rospy.Subscriber('/servo_server/delta_twist_cmds', TwistStamped, self.cmd_callback, queue_size=1)
         self.group.set_pose_reference_frame("world")
         self.group.set_max_velocity_scaling_factor(0.5)
         self.group.set_max_acceleration_scaling_factor(0.5)
@@ -24,7 +23,7 @@ class MoveItController:
         self.linear_scale = 0.02
         self.angular_scale = 0.02
         
-        self.current_cmd = Twist()
+        self.current_cmd = TwistStamped()
         self.cmd_lock = Lock()
         self.executing = False
 
@@ -35,7 +34,6 @@ class MoveItController:
 
     def get_current_pose(self):
         base_frame = "world"
-        # base_frame = "base_link"
         try:
             (trans, rot) = self.tf_listener.lookupTransform(base_frame, self.group.get_end_effector_link(), rospy.Time(0))
             current_pose = Pose()
@@ -64,10 +62,10 @@ class MoveItController:
         while not rospy.is_shutdown():
             with self.cmd_lock:
                 cmd = self.current_cmd
-                self.current_cmd = Twist()  # Reset the command
+                self.current_cmd = TwistStamped()  # Reset the command
 
-            if cmd.linear.x == 0 and cmd.linear.y == 0 and cmd.linear.z == 0 and \
-               cmd.angular.x == 0 and cmd.angular.y == 0 and cmd.angular.z == 0:
+            if cmd.twist.linear.x == 0 and cmd.twist.linear.y == 0 and cmd.twist.linear.z == 0 and \
+               cmd.twist.angular.x == 0 and cmd.twist.angular.y == 0 and cmd.twist.angular.z == 0:
                 self.executing = False
                 return
 
@@ -77,17 +75,17 @@ class MoveItController:
                 continue
             
             # Update position
-            current_pose.position.x += cmd.linear.x * self.linear_scale
-            current_pose.position.y += cmd.linear.y * self.linear_scale
-            current_pose.position.z += cmd.linear.z * self.linear_scale
+            current_pose.position.x += cmd.twist.linear.x * self.linear_scale
+            current_pose.position.y += cmd.twist.linear.y * self.linear_scale
+            current_pose.position.z += cmd.twist.linear.z * self.linear_scale
             
             # Update orientation
             orientation_q = current_pose.orientation
             orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
             (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-            roll += cmd.angular.x * self.angular_scale
-            pitch += cmd.angular.y * self.angular_scale
-            yaw += cmd.angular.z * self.angular_scale
+            roll += cmd.twist.angular.x * self.angular_scale
+            pitch += cmd.twist.angular.y * self.angular_scale
+            yaw += cmd.twist.angular.z * self.angular_scale
             q = quaternion_from_euler(roll, pitch, yaw)
             current_pose.orientation = Pose().orientation
             current_pose.orientation.x, current_pose.orientation.y, current_pose.orientation.z, current_pose.orientation.w = q
---
+++ src/ur_teleop/src/spacemouse_node.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import TwistStamped
+
+class JoystickController:
+    def __init__(self):
+        rospy.init_node('joystick_controller')
+        self.joy_sub = rospy.Subscriber('/spacenav/joy', Joy, self.joy_callback)
+        self.cmd_pub = rospy.Publisher('/servo_server/delta_twist_cmds', TwistStamped, queue_size=1)
+        
+        # Initialize command and smoothing parameters
+        self.current_cmd = TwistStamped()
+        self.smoothing_factor = 0.5 # Adjust this value between 0 and 1
+        
+        # Deadzone parameter
+        self.deadzone = 0.1
+        
+        # Scaling factors
+        self.linear_scale = 0.1  # Max linear velocity (m/s)
+        self.angular_scale = 0.1  # Max angular velocity (rad/s)
+
+        # Get the planning frame from ROS parameter server
+        self.planning_frame = rospy.get_param('/robot_description_planning/planning_frame', 'base_link')
+
+        # Set up timer for continuous command publishing
+        self.publish_rate = 50  # Hz
+        self.timer = rospy.Timer(rospy.Duration(1.0/self.publish_rate), self.publish_command)
+
+        # Flag to determine control mode
+        self.linear_mode = False
+
+    def apply_deadzone(self, value, deadzone):
+        if abs(value) < deadzone:
+            return 0
+        return value
+
+    def joy_callback(self, joy_msg):
+        self.linear_mode = joy_msg.buttons[4]
+        
+        cmd = TwistStamped()
+        cmd.header.stamp = rospy.Time.now()
+        cmd.header.frame_id = self.planning_frame
+        
+        if self.linear_mode:  # Linear mode
+            cmd.twist.linear.x = -self.apply_deadzone(joy_msg.axes[0], self.deadzone) * self.linear_scale
+            cmd.twist.linear.y = self.apply_deadzone(joy_msg.axes[1], self.deadzone) * self.linear_scale
+            cmd.twist.linear.z = self.apply_deadzone(joy_msg.axes[4], self.deadzone) * self.linear_scale
+            cmd.twist.angular.x = 0
+            cmd.twist.angular.y = 0
+            cmd.twist.angular.z = 0
+        else:  # Angular mode
+            cmd.twist.linear.x = 0
+            cmd.twist.linear.y = 0
+            cmd.twist.linear.z = 0
+            cmd.twist.angular.x = -self.apply_deadzone(joy_msg.axes[1], self.deadzone) * self.angular_scale
+            cmd.twist.angular.y = -self.apply_deadzone(joy_msg.axes[0], self.deadzone) * self.angular_scale
+            cmd.twist.angular.z = self.apply_deadzone(joy_msg.axes[3], self.deadzone) * self.angular_scale
+
+        # Apply smoothing
+        for attr in ['linear', 'angular']:
+            for axis in ['x', 'y', 'z']:
+                current_value = getattr(getattr(cmd.twist, attr), axis)
+                last_value = getattr(getattr(self.current_cmd.twist, attr), axis)
+                smoothed_value = self.smoothing_factor * current_value + (1 - self.smoothing_factor) * last_value
+                setattr(getattr(self.current_cmd.twist, attr), axis, smoothed_value)
+
+    def publish_command(self, event):
+        self.current_cmd.header.stamp = rospy.Time.now()
+        self.cmd_pub.publish(self.current_cmd)
+
+if __name__ == '__main__':
+    try:
+        JoystickController()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
